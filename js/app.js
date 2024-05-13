837ad538193ac20f44a3be04ff7ac52b302fc78e
Binglei Cai <bingleicai@gmail.com>
2024-05-13 18:17:48 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const sections = document.querySelectorAll('section');
+    const secBtnContainer = document.querySelector('.controls');
+    const secBtns = document.querySelectorAll('.control');
+    const allSections = document.querySelector('.main-content');
+
+    //add function for each sec-Btn (add class 'active-btn' to section button when clicked and remove from other class list), when button clicked, class 'active-btn' is added
+    for(const secBtn of secBtns){
+        secBtn.addEventListener('click', function() {
+            let currentBtn = document.querySelector('.active-btn');
+            currentBtn.className = currentBtn.className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        //only perform when button is clicked
+        if(id){
+            //remove other section from class 'active'
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+
+            //add according section to class 'active'
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+    
+});
