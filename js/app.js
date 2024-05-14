2ff53b2385c1d85e7601a49f246c67b5fdb22f58
Binglei Cai <bingleicai@gmail.com>
2024-05-14 02:00:54 -0400

    Adds toggle theme button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const secBtnContainer = document.querySelector('.controls');
     const secBtns = document.querySelectorAll('.control');
     const allSections = document.querySelector('.main-content');
+    const themeBtn = document.querySelector('.theme-btn');
 
     //add function for each sec-Btn (add class 'active-btn' to section button when clicked and remove from other class list), when button clicked, class 'active-btn' is added
     for(const secBtn of secBtns){
@@ -26,6 +27,12 @@ document.addEventListener('DOMContentLoaded', function(){
             const element = document.getElementById(id);
             element.classList.add('active');
         }
+    })
+    
+    //toggle theme
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
     })
     
 });
